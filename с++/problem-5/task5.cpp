f466b2ede3524c24ca27b897a8e9dbdb9a01ffb9
Miras <mkaiyrzhan.mk@gmail.com>
2026-02-10 19:01:13 +0500

    feat: add a solution to problem 5

---
+++ с++/problem-5/task5.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a;
+    cin >> a;
+    double V = a * a * a;
+    double S = 6 * (a * a);
+    cout << "V = " << V << endl;
+    cout << "S = " << S;
+    return 0;
+}
