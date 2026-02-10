d01750e5f181519c0482163d1da58e8a000fbe70
Miras <mkaiyrzhan.mk@gmail.com>
2026-02-10 19:06:21 +0500

    fix: change output format for volume calculation in problem 5

--- с++/problem-5/task5.cpp
+++ с++/problem-5/task5.cpp
@@ -5,7 +5,7 @@ int main() {
     cin >> a;
     double V = a * a * a;
     double S = 6 * (a * a);
-    cout << "V = " << V << endl;
+    cout << "V = " << V << "\n";
     cout << "S = " << S;
     return 0;
 }
