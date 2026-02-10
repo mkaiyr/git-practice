8be295da94fce48e5f785a1cdfccaf40507471a0
Miras <mkaiyrzhan.mk@gmail.com>
2026-02-10 18:27:04 +0500

    feat: add a solution to problem 3

---
+++ с++/problem-3/task3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cin >> a >> b;
+    int S = a * b;
+    int P = 2 * (a + b);
+    cout << "S = " << S << endl;
+    cout << "P = " << P;
+    return 0;
+}
