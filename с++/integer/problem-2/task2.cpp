42f780552ade14193252b1fe0125b6a6e4bbc81d
Miras <mkaiyrzhan.mk@gmail.com>
2026-02-11 19:04:15 +0500

    feat: add a solution to problem 2 int

---
+++ с++/integer/problem-2/task2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int M;
+    cin >> M;
+    int tonna = M / 1000;
+    cout << "result = " << tonna << " tonna" << "\n";
+    return 0;
+}
