9a4ca7c2fd8509be2e641edf3663ccb164269509
Miras <mkaiyrzhan.mk@gmail.com>
2026-02-11 17:31:54 +0500

    feat: add a solution to problem 8

---
+++ с++/problem-8/task8.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    double a , b;
+    cin >> a >> b;
+    double arif = (a + b) / 2;
+    cout << "Arifmeticheskoye sredneye = " << arif << "\n";
+    return 0;
+}
